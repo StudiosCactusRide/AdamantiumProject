4b4d56ea1ab87a317a1e2325eda121efc9ad8727
StudiosCactusRide <studioscactusride@gmail.com>
2016-03-09 18:20:34 -0300

    Nuvens
    As nuvens são geradas aleatóriamente e se movimentam com velocidades
    diferentes

---
+++ SplashBall/Assets/Scripts/Cloud.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	private float vel;
+
+
+	// Use this for initialization
+	void Start () {
+		vel = Random.Range (0.8f, 5.0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		MoveCloud ();
+	}
+
+	void MoveCloud(){
+		transform.Translate (vel * Time.deltaTime, 0, 0);
+
+		if (transform.position.x > 36) {
+			Destroy (gameObject);
+		}
+	}
+}
